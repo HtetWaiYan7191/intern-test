cbdd125ac79d71f1cc855a9d0ca943977d75b3fa
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-03-13 23:03:30 +0630

    add custom colors in tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'footer-black': '#0D0D0D',
+        'purple-linear': '#AB96F8',
+        'pink-linear': '#FF99D0',
+      }
+    },
   },
   plugins: [],
 }
