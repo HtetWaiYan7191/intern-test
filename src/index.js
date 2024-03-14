25642c2960a880c77ece49c90bf8a902974a1a17
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-03-14 23:00:47 +0630

    Add accordion function in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const mobileMenuIcon = document.getElementById('menu-icon');
 const mobileMenuList = document.getElementById('mobile-menu-list');
 const closeIcon = document.getElementById('close-icon');
+const upIcons = document.getElementsByClassName('up-icons');
+
+for (let i = 0; i < upIcons.length; i++) {
+  upIcons[i].addEventListener('click', (e) => {
+    const questionBox = e.target.closest('.question-box');
+    const answer = questionBox.querySelector('.answer');
+    if (answer) {
+      answer.classList.toggle('hidden');
+      e.target.classList.toggle('fa-chevron-up');
+      e.target.classList.toggle('fa-chevron-down');
+    }
+  });
+}
 
 function closeMobileMenu() {
   mobileMenuList.classList.add('hidden');
