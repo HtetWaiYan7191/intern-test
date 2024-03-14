81f4fd8e5b280d28b5991ef537bb90c06cbbeb12
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-03-14 09:16:58 +0630

    Complete main section page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         'footer-black': '#0D0D0D',
+        'title': '#18181B',
         'gray': '#000000',
         'btn-black': '#18181C',
         'purple-linear': '#AB96F8',
